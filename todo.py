9d0d68302149f10861fa0603a0a86a133991732c
Armand Foucault <armand.foucault@telecom-bretagne.eu>
2020-12-30 14:31:21 +0100

    Write rules skeleton

---
+++ todo.py
@@ -0,0 +1,219 @@
+import dataclasses as dc
+import datetime as dt
+import re
+
+import neovim
+import sly
+
+
+@dc.dataclass
+class TodoItem:
+    done: bool = False
+    description: str = ""
+    project_tags: [str] = None
+    context_tags: [str] = None
+    metadata: {str: str} = None
+    creation_date: dt.date = None
+    completion_date: dt.date = None
+
+
+class TodoLexer(sly.Lexer):
+    tokens = {
+        # X,
+        # PRIORITY,
+        DATE,
+        PROJECT_TAG,
+        CONTEXT_TAG,
+        WORD,
+        METADATA,
+    }
+
+    ignore = " \t\n\r"
+
+    # X = r"x"
+    # PRIORITY = r"\([A-Z]\)"
+    DATE = r"\d{4}-\d{2}-\d{2}"
+    PROJECT_TAG = r"\+\w+"
+    CONTEXT_TAG = r"@\w+"
+    METADATA = r"\w+:\w+"
+    WORD = r"\w+"
+
+    def METADATA(self, token):
+        key, value = token.value.split(":")
+        token.value = {key: value}
+        return token
+
+    def DATE(self, token):
+        token.value = dt.date.fromisoformat(token.value)
+        return token
+
+
+class TodoParser(sly.Parser):
+    tokens = TodoLexer.tokens
+
+    start = "todo"
+
+    # @_("X desctiption")
+    # def todo(self, p):
+    #     pass
+
+    # @_("PRIORITY desctiption")
+    # def todo(self, p):
+    #     pass
+
+    # @_("DATE DATE description")
+    # def todo(self, p):
+    #     todo_item = p.description
+    #     todo_item.completion_date = p.DATE0
+    #     todo_item.creation_date = p.DATE1
+    #     return todo_item
+
+    # @_("DATE description")
+    # def todo(self, p):
+    #     todo_item = p.description
+    #     todo_item.creation_date = p.DATE
+    #     return todo_item
+
+    @_("header description")
+    def todo(self, p):
+        pass
+        # return p.description
+
+    @_("description")
+    def todo(self, p):
+        return p.description
+
+    @_("X PRIORITY")
+    def header(self, p):
+        pass
+
+    @_("X dates")
+    def header(self, p):
+        pass
+
+    @_("X PRIORITY dates")
+    def header(self, p):
+        pass
+
+    @_("PRIORITY dates")
+    def header(self, p):
+        pass
+
+    @_("X")
+    def header(self, p):
+        pass
+
+    @_("PRIORITY")
+    def header(self, p):
+        pass
+
+    @_("dates")
+    def header(self, p):
+        pass
+
+    @_("DATE")
+    def dates(self, p):
+        pass
+
+    @_("DATE DATE")
+    def dates(self, p):
+        pass
+
+    @_("words tags")
+    def description(self, p):
+        return TodoItem(
+            description=p.words,
+            context_tags=p.tags.get("context_tags", []),
+            project_tags=p.tags.get("project_tags", []),
+            metadata=p.tags.get("metadata", {}),
+        )
+
+    @_("words")
+    def description(self, p):
+        return TodoItem(description=p.words)
+
+    @_("PROJECT_TAG tags")
+    def tags(self, p):
+        result = p.tags
+        result.setdefault("project_tags", []).append(p.PROJECT_TAG)
+        return result
+
+    @_("CONTEXT_TAG tags")
+    def tags(self, p):
+        result = p.tags
+        result.setdefault("context_tags", []).append(p.CONTEXT_TAG)
+        return result
+
+    @_("METADATA tags")
+    def tags(self, p):
+        result = p.tags
+        key, value = p.METADATA.split(":")
+        result.setdefault("metadata", {})[key] = value
+        return result
+
+    @_("PROJECT_TAG")
+    def tags(self, p):
+        return {"project_tags": [p.PROJECT_TAG]}
+
+    @_("CONTEXT_TAG")
+    def tags(self, p):
+        return {"context_tags": [p.CONTEXT_TAG]}
+
+    @_("METADATA")
+    def tags(self, p):
+        return {"metadata": p.METADATA}
+
+    @_("WORD words")
+    def words(self, p):
+        return [p.WORD] + p.words
+
+    @_("WORD")
+    def words(self, p):
+        return [p.WORD]
+
+
+TODO_REGEX = re.compile(
+    r"(?:(?P<done>^|^x) )?\s*"
+    r"(?:\(?P<priority>[A-Z]\) )?\s*"
+    r"(?:(?P<completion_date>\d{4}-\d{2}-\d{2}) )?\s*"
+    r"(?:(?P<creation_date>\d{4}-\d{2}-\d{2}) )?\s*"
+)
+
+
+def parse_todo_line(line):
+    lexer = TodoLexer()
+    parser = TodoParser()
+    result = parser.parse(lexer.tokenize(line))
+    return result
+    # return {
+    #     "done": False,
+    #     "priority": None,
+    #     "completion_date": None,
+    #     "creation_date": None,
+    #     "description": "",
+    #     "project_tags": set(),
+    #     "context_tags": set(),
+    #     "metadata": {},
+    # }
+
+
+
+
+@neovim.plugin
+class TodoPlugin(object):
+    def __init__(self, nvim: neovim.Nvim):
+        self._nvim = nvim
+
+    @neovim.command("TodoHello")
+    def todo_hello(self):
+        self._nvim.current.line = "hello"
+
+    @neovim.autocmd("BufEnter", pattern="*todo.txt")
+    def on_enter(self):
+        pass
+
+
+if __name__ == "__main__":
+    import sys
+    line = " ".join(sys.argv[1:])
+    print(parse_todo_line(line))
